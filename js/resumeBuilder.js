9af5c2b45c98d09f92cd491b1d7ae18b51b9d7b6
Ralph <beresfordjunior@me.com>
2015-01-15 17:33:32 +0000

    added initial bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,16 @@
+var bio = {
+  "name": "Ralph Reid",
+  "role": "Software Engineer",
+  "contact info": "ralphreidjr@me.com",
+  "pictureURL": "images/me.jpg",
+  "welcome message": "put welcolme message here",
+  "skills": ["javascript", "html", "ruby", "php"]
+};
 
-var formattedName = HTMLheaderName.replace('%data%', 'Ralph Reid');
-
-var formattedRole = HTMLheaderRole.replace('%data%', 'Software Engineer');
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 
 $("#header").prepend(formattedName);
 $("#header").append(formattedRole);
+
+// console.log(bio.skills);
