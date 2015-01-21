d8cf4e72f78f3fc681d187a24e056e33b606bb52
Ralph <beresfordjunior@me.com>
2015-01-21 07:46:37 +0000

    updated json

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,29 +11,78 @@ var bio = {
   },
   "bioPic": "images/me.jpg",
   "welcome message": "put welcolme message here",
-  "skills": ["javascript", "html", "ruby", "php"]
+  "skills": [ "javascript", "html", "ruby", "php" ]
 };
 
-var work = {};
-work.position = "Software Engineer";
-work.employer = "POD Point"
-work.years = 1;
+var education = {
+  "schools": [
+    {
+      "name": "University of Illinois",
+      "city": "Champaign, IL, USA",
+      "major": [ "Chemistry", "Economics" ],
+      "dates": 1998,
+      "url": "http://google.com"
+    },
+    {
+      "name": "Xavier University",
+      "city": "Cincinnati, OH, USA",
+      "major": "MBA",
+      "dates": 2003,
+      "url": "http://google.com"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "akjfdalskjf",
+      "school": "Upcase",
+      "url": "t.co",
+      "courses": [ "TBC1", "TBC2" ],
+      "topics": [ "TBC1", "TBC2" ],
+      "url": "http://google.com",
+      "dates": 2014
+    },
+    {
+      "title": "akjfdalskjf",
+      "school": "Codeschool",
+      "url": "t.co",
+      "courses": [ "TBC1", "TBC2" ],
+      "topics": [ "TBC1", "TBC2" ],
+      "url": "http://google.com",
+      "dates": 2014
+    }
+  ]
+};
 
+var work = {
+  "jobs": [
+    {
+      "employer": "POD Point",
+      "title": "Software Engineer",
+      "location": "London, UK",
+      "dateStart": "1-2013",
+      "dateEnd": "present",
+      "description": "lkajsdslfkjldskjfdslkj"
+    }
+  ]
+};
 
-var education = {};
-education["name"]="University of Illinois";
-education["years"] = "1993 - 1998";
-education["city"] = "Champaign, IL, USA"
-
-
-var formattedName = HTMLheaderName.replace("%data%", bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-
-$("#header").prepend(formattedName);
-$("#header").append(formattedRole);
-
-$("#main").append(work["position"]);
-$("#main").append(education.name);
-
-
-// console.log(bio.skills);
+var project = {
+  "projects": [
+    {
+      "title": "TBCCCCCC",
+      "dates": 2014,
+      "description": "adsfdsffdssfsdfafddsafd",
+      "images": [ "http://google.com", "http://google.com" ],
+      "githubUrl": "http://google.com",
+      "url": "http://google.com"
+    },
+    {
+      "title": "TBCCCCCC",
+      "dates": 2014,
+      "description": "adsfdsffdssfsdfafddsafd",
+      "images": [ "http://google.com", "http://google.com" ],
+      "githubUrl": "http://google.com",
+      "url": "http://google.com"
+    }
+  ]
+};
