eb4e5d31192da2e0c882bca5a065c908d45a156b
Ralph <beresfordjunior@me.com>
2015-01-29 20:47:06 +0000

    continuing to work on encapsulated functions for display projects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,83 +1,83 @@
 var bio = {
-	"name": "Ralph Reid",
-	"role": "Full-Stack Engineer",
-	"contacts": {
-		"mobileUK": "+447985057831",
-		"mobileUSA": "enter here",
-		"github": "ralphreid",
-		"twitter": "TBC",
-		"linkedin": "TBC",
-		"location": "San Fransico, Seatle & London"
-	},
-	"bioPic": "images/me.jpg",
-	"welcome message": "put welcolme message here",
-	"skills": ["javascript", "html", "ruby", "php"]
+  "name": "Ralph Reid",
+  "role": "Full-Stack Engineer",
+  "contacts": {
+    "mobileUK": "+447985057831",
+    "mobileUSA": "enter here",
+    "github": "ralphreid",
+    "twitter": "TBC",
+    "linkedin": "TBC",
+    "location": "San Fransico, Seatle & London"
+  },
+  "bioPic": "images/me.jpg",
+  "welcome message": "put welcolme message here",
+  "skills": ["javascript", "html", "ruby", "php"]
 };
 
 var education = {
-	"schools": [{
-		"name": "University of Illinois",
-		"city": "Champaign, IL, USA",
-		"major": ["Chemistry", "Economics"],
-		"dates": 1998,
-		"url": "http://google.com"
-	}, {
-		"name": "Xavier University",
-		"city": "Cincinnati, OH, USA",
-		"major": "MBA",
-		"dates": 2003,
-		"url": "http://google.com"
-	}],
-	"onlineCourses": [{
-		"title": "akjfdalskjf",
-		"school": "Upcase",
-		"url": "t.co",
-		"courses": ["TBC1", "TBC2"],
-		"topics": ["TBC1", "TBC2"],
-		"url": "http://google.com",
-		"dates": 2014
-	}, {
-		"title": "akjfdalskjf",
-		"school": "Codeschool",
-		"url": "t.co",
-		"courses": ["TBC1", "TBC2"],
-		"topics": ["TBC1", "TBC2"],
-		"url": "http://google.com",
-		"dates": 2014
-	}]
+  "schools": [{
+    "name": "University of Illinois",
+    "city": "Champaign, IL, USA",
+    "major": ["Chemistry", "Economics"],
+    "dates": 1998,
+    "url": "http://google.com"
+  }, {
+    "name": "Xavier University",
+    "city": "Cincinnati, OH, USA",
+    "major": "MBA",
+    "dates": 2003,
+    "url": "http://google.com"
+  }],
+  "onlineCourses": [{
+    "title": "akjfdalskjf",
+    "school": "Upcase",
+    "url": "t.co",
+    "courses": ["TBC1", "TBC2"],
+    "topics": ["TBC1", "TBC2"],
+    "url": "http://google.com",
+    "dates": 2014
+  }, {
+    "title": "akjfdalskjf",
+    "school": "Codeschool",
+    "url": "t.co",
+    "courses": ["TBC1", "TBC2"],
+    "topics": ["TBC1", "TBC2"],
+    "url": "http://google.com",
+    "dates": 2014
+  }]
 };
 
 var work = {
-	"jobs": [{
-		"employer": "POD Point",
-		"title": "Software Engineer",
-		"location": "London, UK",
-		"dateStart": "1-2013",
-		"dateEnd": "present",
-		"description": "lkajsdslfkjldskjfdslkj"
-	}]
+  "jobs": [{
+    "employer": "POD Point",
+    "title": "Software Engineer",
+    "location": "London, UK",
+    "dateStart": "1-2013",
+    "dateEnd": "present",
+    "description": "lkajsdslfkjldskjfdslkj"
+  }]
 };
 
 var project = {
-	"projects": [{
-		"title": "TBCCCCCC",
-		"dates": 2014,
-		"description": "adsfdsffdssfsdfafddsafd",
-		"images": [
-			"http://google.com",
-			"http://google.com"
-		],
-		"githubUrl": "http://google.com",
-		"url": "http://google.com"
-	}, {
-		"title": "TBCCCCCC",
-		"dates": 2014,
-		"description": "adsfdsffdssfsdfafddsafd",
-		"images": [
-			"http://google.com",
-			"http://google.com"
-		],
-		"githubUrl": "http://google.com",
-		"url": "http://google.com"
-	}]
+  "projects": [{
+    "title": "TBCCrrC",
+    "dates": 2014,
+    "description": "adsfdsffdssfsdfafddsafd",
+    "images": [
+      "http://placehold.it/350x150",
+      "http://placehold.it/350x150"
+    ],
+    "githubUrl": "http://google.com",
+    "url": "http://google.com"
+  }, {
+    "title": "TBCCCCCC",
+    "dates": 2014,
+    "description": "adsfdsffdssfsdfafddsafd",
+    "images": [
+      "http://placehold.it/350x150",
+      "http://placehold.it/350x150"
+    ],
+    "githubUrl": "http://google.com",
+    "url": "http://google.com"
+  }]
 };
