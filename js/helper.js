d8bbb0f324b4a9b8984ecfbbda533a1793f9c90b
Ralph <beresfordjunior@me.com>
2015-02-09 23:14:27 +0000

    refactored

--- js/helper.js
+++ js/helper.js
@@ -1,19 +1,3 @@
-/*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
-
-/* added*/
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr/>';
 
@@ -82,25 +66,6 @@ $(document).ready(function() {
 		$('#name').html(iName);
 	});
 });
-
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
-*/
-clickLocations = [];
-
-function logClicks(x, y) {
-	clickLocations.push({
-		x: x,
-		y: y
-	});
-	console.log('x location: ' + x + '; y location: ' + y);
-}
-
-$(document).click(function(loc) {
-	// your code goes here!
-});
-
-
 
 /*
 This is the fun part. Here's where we generate the custom Google Map for the website.
