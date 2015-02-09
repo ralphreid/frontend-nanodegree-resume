c66cbeea0e7b09fd96333b490deabd675c23b2ea
Ralph <beresfordjunior@me.com>
2015-02-09 23:35:29 +0000

    added click location logger

--- js/helper.js
+++ js/helper.js
@@ -57,6 +57,27 @@ var googleMap = '<div id="map"></div>';
 
 
 /*
+The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
+*/
+clickLocations = [];
+
+function logClicks(x, y) {
+	clickLocations.push({
+		x: x,
+		y: y
+	});
+	console.log('x location: ' + x + '; y location: ' + y);
+}
+
+$(document).click(function(loc) {
+	var x = loc.pageX;
+	var y = loc.pageY;
+	logClicks(x, y);
+});
+
+
+
+/*
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run.
 Don't delete! It hooks up your code to the button you'll be appending.
 */
